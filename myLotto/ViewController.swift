161061e484b9fe10e84a0c6ed090c05a2615cdb6
장성구 <ggrty10@gmail.com>
2020-03-26 20:53:42 +0900

    create project

---
+++ myLotto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myLotto
+//
+//  Created by 장성구 on 2020/03/26.
+//  Copyright © 2020 com.sg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
