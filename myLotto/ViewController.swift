c0f4b86502835819f0ab19a5e0217d17dec33374
장성구 <ggrty10@gmail.com>
2020-03-26 21:16:46 +0900

    레이블 및 버튼 컨트롤러에 연결

--- myLotto/ViewController.swift
+++ myLotto/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /* 각 번호 표시 레이블 연결 */
+    @IBOutlet weak var showPrint1: UILabel!
+    @IBOutlet weak var showPrint2: UILabel!
+    @IBOutlet weak var showPrint3: UILabel!
+    @IBOutlet weak var showPrint4: UILabel!
+    @IBOutlet weak var showPrint5: UILabel!
+    @IBOutlet weak var showPrint6: UILabel!
+    
+    /* 전체 결과 표시 레이블 연결 */
+    @IBOutlet weak var totalPrint: UILabel!
+    
+    /* 버튼 연결 및 클릭시 동작 */
+    @IBAction func runButton(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
